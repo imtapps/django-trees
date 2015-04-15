4c09b3432f21cadc111ef9a0a68c0b99f409c4d7
mattjmorrison <mattjmorrison@mattjmorrison.com>
2015-04-15 09:48:40 -0500

    POC

--- django_trees/tests/test_json.py
+++ django_trees/tests/test_json.py
@@ -1,7 +1,35 @@
 from collections import defaultdict
-from pprint import pprint
 from django import test
 from django_trees.tests.test_app.models import Node
+
+
+class Directory(object):
+
+    def __init__(self, documents, folders):
+        self.documents = defaultdict(list)
+        for doc in documents:
+            self.documents[doc.get('folder')].append(doc)
+        self.folders = {x: {
+            'documents': [], 'folders': [], 'name': y, 'parent': z
+        } for x, y, z in folders.values_list('id', 'name', '_parent')}
+
+    def folder_documents(self, folder):
+        return self.documents.get(folder, [])
+
+    def doit(self):
+        folders = []
+
+        for key, node in self.folders.items():
+            if node['parent']:
+                self.folders[node['parent']]['folders'].append(node)
+            else:
+                folders.append(node)
+            node['documents'] = self.folder_documents(key)
+
+        return {
+            'folders': folders,
+            'documents': self.folder_documents(None)
+        }
 
 
 class JsonTests(test.TestCase):
@@ -23,53 +51,48 @@ class JsonTests(test.TestCase):
         ]
 
     def test_create_nested_json(self):
-        docs = defaultdict(list)
-        for doc in self.documents:
-            docs[doc.get('folder')].append(doc)
-
-        folders = []
-        data = {x: {
-            'folders': [], 'name': y, 'parent': z
-        } for x, y, z in Node.objects.all().values_list('id', 'name', '_parent')}
-
-        for key, node in data.items():
-            if node['parent']:
-                data[node['parent']]['folders'].append(node)
-            else:
-                folders.append(node)
-            node['documents'] = docs.get(key, [])
-
-        self.assertStructureCreated({
-            'folders': folders,
-            'documents': docs.get(None, [])
-        })
+        directory = Directory(self.documents, Node.objects.all())
+        self.assertStructureCreated(directory.doit())
 
     def assertStructureCreated(self, result):
         expected = {
-            'documents': [{'id': 1, 'name': 'doc1'}],
+            'documents': [{
+                'id': 1, 'name': 'doc1'
+            }],
             'folders': [{
+                'name': u'a',
+                'parent': None,
                 'documents': [
                     {'folder': 1, 'id': 2, 'name': 'doc2'},
                     {'folder': 1, 'id': 3, 'name': 'doc3'}
                 ],
                 'folders': [{
+                    'name': u'b',
+                    'parent': 1,
                     'documents': [{
                         'folder': 2, 'id': 4, 'name': 'doc4'
                     }],
                     'folders': [{
-                        'documents': [], 'folders': [], 'name': u'e', 'parent': 2
+                        'name': u'e',
+                        'parent': 2,
+                        'documents': [],
+                        'folders': []
                     }],
-                    'name': u'b', 'parent': 1
                 }, {
+                    'name': u'c',
+                    'parent': 1,
                     'documents': [{
                         'folder': 3, 'id': 5, 'name': 'doc5'
                     }, {
                         'folder': 3, 'id': 6, 'name': 'doc6'
                     }],
                     'folders': [{
-                        'documents': [], 'folders': [], 'name': u'd', 'parent': 3
+                        'name': u'd',
+                        'parent': 3,
+                        'documents': [],
+                        'folders': [],
                     }],
-                    'name': u'c', 'parent': 1
                 }],
-                'name': u'a', 'parent': None}]}
+            }]
+        }
         self.assertEqual(expected, result)
